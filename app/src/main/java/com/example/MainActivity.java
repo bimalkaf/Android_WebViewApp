df97a1a6d3f3c4352f47cde78dc3a4a46c9a2476
Easy Tuto <tutoeasy1@gmail.com>
2021-07-17 00:23:40 +0530

    html,css,js

---
+++ app/src/main/java/com/example/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = (WebView) findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_asset/index.html");
+
+    }
+}
